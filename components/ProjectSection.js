0172474a4dd99fb4e5dc19dd346b822bae51cbc4
Vivian Chen <vivianchen@ca.ibm.com>
2021-09-03 10:33:23 -0700

    add some responsive classes

--- components/ProjectSection.js
+++ components/ProjectSection.js
@@ -1,11 +1,11 @@
 function ProjectSection(props) {
   //   console.log(props.title);
   return (
-    <div className="row flex items-center flex-grow">
-      <div className="col col-sm-8 col-lg-3 col-md-6 ">
+    <div className="row flex flex-col-reverse md:flex-row mb-40 md:mb-0 md:items-center md:flex-grow">
+      <div className="col-sm-12 col-lg-3 col-md-6  ">
         <div className="flex items-center">
           <div className="">
-            <h3 className="text-2xl font-bold">{props.title}</h3>
+            <h3 className="text-2xl font-bold mt-10 md:mt-0">{props.title}</h3>
             <p className="mt-4 text-xl max-w-lg">{props.desc}</p>
             <a
               href={props.url}
@@ -17,7 +17,7 @@ function ProjectSection(props) {
           </div>
         </div>
       </div>
-      <div className="col col-sm-8 col-lg-3 col-md-6 col-lg-offset-2">
+      <div className="col-sm-12 col-lg-3 col-md-6 col-lg-offset-2">
         <img alt="ibm logo" src={props.imgSrc} />
       </div>
     </div>
